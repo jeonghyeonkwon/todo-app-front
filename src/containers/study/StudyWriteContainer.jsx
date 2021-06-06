a4ba40b945be4bed72950612010fbce14bfbfaa6
jeonghyeonkwon <givejeong2468@gmail.com>
2021-06-07 00:44:59 +0900

    스터디 게시글 쓰기 컴포넌트 수정

--- src/containers/study/StudyWriteContainer.jsx
+++ src/containers/study/StudyWriteContainer.jsx
@@ -1,7 +1,56 @@
-import React from 'react';
+import React, { useState, useCallback, useEffect } from 'react';
 import CreateBoard from '../../components/main/CreateBoard';
-const StudyWriteContainer = () => {
-  return <CreateBoard type="study"></CreateBoard>;
-};
+import { withRouter } from 'react-router-dom';
+const StudyWriteContainer = ({ location }) => {
+  const skill = location.pathname.split('/')[2];
+  const [form, setForm] = useState({
+    title: '',
+    contents: '',
+    applicant: 0,
+    programmingType: null,
+  });
+  const [tag, setTag] = useState([]);
 
-export default StudyWriteContainer;
+  const onChange = useCallback(
+    (e) => {
+      const { name, value } = e.target;
+      if (name === 'applicant' && value < 0) {
+        setForm({
+          ...form,
+          applicant: 0,
+        });
+      } else {
+        setForm({
+          ...form,
+          [name]: value,
+        });
+      }
+    },
+    [form],
+  );
+  const onChangeType = (e, v) => {
+    if (v.length > 2) {
+      const [trash, ...val] = v;
+      setTag([...val]);
+    } else {
+      setTag([...v]);
+    }
+  };
+  useEffect(() => {
+    console.log(form);
+  }, [form]);
+  const onSubmit = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <CreateBoard
+      boardType="study"
+      skill={skill}
+      onChange={onChange}
+      onChangeType={onChangeType}
+      form={form}
+      tag={tag}
+    />
+  );
+};
+export default withRouter(StudyWriteContainer);
